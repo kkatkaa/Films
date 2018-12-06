f3862df85283b1a969496683e226e56bb1e6f77c
Kasia <katka36@tlen.pl>
2018-12-06 12:57:37 +0100

    Modify routes.rb connect movies and comments model

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,8 @@ Rails.application.routes.draw do
 
   root 'welcome#index'
   get 'welcome/index'
-  resources :movies
+  resources :movies do
+    resources :comments
+end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
