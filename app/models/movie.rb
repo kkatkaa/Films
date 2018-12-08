90fa2af82310208ec68587dcc02898a92dbf26ed
Kasia <katka36@tlen.pl>
2018-12-08 20:08:43 +0100

    Change the language in app to English

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,4 +1,4 @@
 class Movie < ApplicationRecord
   validates :tytuł, presence: true, length: { minimum: 1 }
-  has_many :comments,  dependent: :destroy 
+  has_many :comments,  dependent: :destroy
 end
