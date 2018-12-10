af2b65eb9e4dfdb1560725a23912cbee156b1813
Kasia <katka36@tlen.pl>
2018-12-10 22:28:15 +0100

    Remove belongs_to director inscription

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,6 +1,5 @@
 class Movie < ApplicationRecord
   validates :tytuł, presence: true, length: { minimum: 1 }
-  belongs_to :director 
-  has_many :comments,  dependent: :destroy
 
+  has_many :comments,  dependent: :destroy
 end
