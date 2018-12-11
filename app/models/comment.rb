64ed47e295f450577688f39bf1cb3841bad7f6d0
Kasia <katka36@tlen.pl>
2018-12-11 11:20:54 +0100

    Modify comments and make possibility to rate a movie

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,6 +1,7 @@
 class Comment < ApplicationRecord
   validates :commenter, presence: true, uniqueness: true, format: /@/
   validates :body, presence: true, length: {in: 5..500}
+  validates :rating, numericality: true, :inclusion => 1..10
 
   belongs_to :movie
 end
