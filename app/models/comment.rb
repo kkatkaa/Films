a7089df066aad579e09bdf3fb9d92a3d25931a50
Kasia <katka36@tlen.pl>
2018-12-28 13:13:44 +0100

    Fix name of commenter

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,6 +1,6 @@
 class Comment < ApplicationRecord
   include ActiveModel::Validations
-  validates :user, presence: true, uniqueness: true, format: /@/
+  validates :user, presence: true, uniqueness: true
   validates :body, presence: true, length: {in: 5..500}
   validates :rating, numericality: true, :inclusion => 1..10
 
